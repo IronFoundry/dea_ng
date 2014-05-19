9b16f94e373dd958119568f36eb3b4a10a0d7b64
Bob Brumfield <bob.brumfield@gmail.com>
2014-05-19 12:28:59 -0700

    Moved scope of rescue handling to cover main loop

--- bin/dea_winsvc.rb
+++ bin/dea_winsvc.rb
@@ -18,52 +18,50 @@ unless ARGV.size == 1
   abort("Usage: dea_winsvc.rb <config path>")
 end
 
-begin
-  class DeaDaemon < Daemon
+class DeaDaemon < Daemon
 
-    def service_init
-      begin
-        config = YAML.load_file(ARGV[0])
+  def service_init
+    begin
+      config = YAML.load_file(ARGV[0])
 
-        # Ensure pid file is deleted or else service won't start
-        pid_file = config["pid_filename"]
-        FileUtils.rm_f(pid_file)
+      # Ensure pid file is deleted or else service won't start
+      pid_file = config["pid_filename"]
+      FileUtils.rm_f(pid_file)
 
-        @bootstrap = Dea::Bootstrap.new(config)
-      rescue => e
-        abort("ERROR: Failed loading config: #{e}")
-      end
+      @bootstrap = Dea::Bootstrap.new(config)
+    rescue => e
+      abort("ERROR: Failed loading config: #{e}")
     end
-
-    def service_main(*args)
-
-      EM.epoll
-
-      begin
-        EM.run {
-          @bootstrap.setup
-          @bootstrap.start
-        }
-      rescue => e
-        @bootstrap.logger.error "DEA Windows Server failed", error: e
-        exit!
-      end
-
-      stop_em
-    end
-
-    def service_stop
-      stop_em
-    end
-
-    def stop_em
-      EM.next_tick { EM.stop }
-    end
-
   end
 
-  DeaDaemon.mainloop
+  def service_main(*args)
 
+    EM.epoll
+
+    begin
+      EM.run {
+        @bootstrap.setup
+        @bootstrap.start
+      }
+    rescue => e
+      @bootstrap.logger.error "DEA Windows Server failed", error: e
+      exit!
+    end
+
+    stop_em
+  end
+
+  def service_stop
+    stop_em
+  end
+
+  def stop_em
+    EM.next_tick { EM.stop }
+  end
+end
+
+begin
+  DeaDaemon.mainloop
 
 rescue
   # Handle any outside exceptions to help ensure t
